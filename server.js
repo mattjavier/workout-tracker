61b5444a5d769d7b351f09a7a802b7cbb260e4bc
Matt Javier <matthewrjavier@gmail.com>
2020-09-10 10:59:25 -0700

    removed morgan

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const { join } = require('path')
-const morgan = require('morgan')
 
 const app = express()
-
-// app.use(morgan('dev'))
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
