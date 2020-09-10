573ed7c2f77205a09dbf168e2efe1f5427df1951
Matt Javier <matthewrjavier@gmail.com>
2020-09-10 11:28:41 -0700

    comments

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -14,6 +14,7 @@ router.get('/workouts/range', (req, res) => {
     .catch(err => console.log(err))
 })
 
+// POST new workout
 router.post('/workouts', (req, res) => {
   Workout.create(req.body)
     .then(workout => res.json(workout))
